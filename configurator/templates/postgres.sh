71b3b455158e9197d3f5e4bea9fe656511b4d7d9
Stephane Poss <stephane.poss@airnavigation.aero>
2017-03-24 13:59:03 +0100

    make sure to be in a neutral zone

--- configurator/templates/postgres.sh
+++ configurator/templates/postgres.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 export LC_ALL=$LANG
+cd /tmp
 createuser {{ username }} -P
 cuser=$?
 if [ ${cuser} != "0" ]; then
    exit ${cuser};
 fi
 createdb {{ dbname }} -O {{ username }}
+cd -
 exit $?;
